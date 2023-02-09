4be2ed9898f0ad641a774120110c925fb16d7898
gabi49 <94904151+gabi49@users.noreply.github.com>
2023-02-09 10:53:33 +0000

    new API

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import sys
+from flask import Flask
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def mytransaction():
+	return "hello"
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+             print("Passed argument not supported ! Supported argument: check_syntax")
+             exit(1)
+
+    app.run(debug=True)
