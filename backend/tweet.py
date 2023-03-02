3546e7daa55a3ea054d9d642edf59555d2dba761
gabi49 <94904151+gabi49@users.noreply.github.com>
2023-03-02 11:39:32 +0000

    cht

--- backend/tweet.py
+++ backend/tweet.py
@@ -10,7 +10,7 @@ users_db = redis.Redis(host='localhost', port=6379, db=1, decode_responses=True)
 
 @app.route('/tweets', methods=['GET'])
 def get_tweets():
-    return jsonify({'tweets': tweets})
+    return jsonify(tweets)
 
 
 @app.route('/Addtweets', methods=['POST'])
