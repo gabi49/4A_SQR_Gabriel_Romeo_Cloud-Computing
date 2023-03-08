b20c2c34c5d2d5d967b395a6f0a900ec21334b78
gabi49 <94904151+gabi49@users.noreply.github.com>
2023-03-08 21:43:19 +0000

    cht

--- backend/tweet.py
+++ backend/tweet.py
@@ -1,31 +1,52 @@
 from flask import Flask, request, jsonify
+import json
 import sys
 import redis
+import time
 
 
 app = Flask(__name__)
-tweets = []
+
 tweets_db = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 users_db = redis.Redis(host='localhost', port=6379, db=1, decode_responses=True)
 
+## Affichier tous les tweets
 @app.route('/')
-def mytransaction():
+def mytweetList():
+    tweets = []
+    for key in tweets_db.scan_iter("tweet:*"):
+        tweet = tweets_db.get(key)
+        tweet = json.loads(tweet)
+        tweets.append(tweet)
     return jsonify(tweets)
 
-@app.route('/tweets', methods=['GET'])
-def get_tweets():
-    return jsonify(tweets)
-
-
+## Enregistrer un tweet dans Redis
 @app.route('/Addtweets', methods=['POST'])
 def create_tweet():
-    tweet = request.json
-    newtweet = {
-		'author' : tweet['username'],
-		'tweet' : tweet['tweet']
-	}
-    tweets.append(newtweet)
-    return "tweet reussi!!!"
+    username = request.json.get('username')
+    message = request.json.get('tweet')
+    timestamp = str(time.time())
+    tweet = {
+            'author': username,
+            'tweet': message
+        }
+    #tweet_key = f'tweet:{timestamp}'
+    tweets_db.set(timestamp, json.dumps(tweet))
+    #users_key = f'user:{username}'
+    users_db.lpush(username, timestamp)
+    return "magnifique"
+
+## Recuperer les tweets d'un etuilisateur grace à son pseudo
+@app.route('/tweets/<username>', methods=['GET'])
+def get_user_tweets(username):
+
+    # Récupère tous les tweets d'un utilisateur
+    user_tweet_keys = users_db.get(username)
+    user_tweets = []
+    for key in user_tweet_keys:
+        tweet = json.loads(tweets_db.get(key))
+        user_tweets.append(tweet)
+    return jsonify(user_tweets)
 
 
 
