3f3bb0478a106dac3045e8096d2077b62d684e08
ROMEO KOULARAMBAYE MBAINGUINAM <49437899+ROMEO2023@users.noreply.github.com>
2023-03-31 20:00:11 +0200

    Update twitter.js
    new front js

--- frontend/twitter.js
+++ frontend/twitter.js
@@ -1,28 +1,22 @@
-// Récupérer le formulaire d'écriture de tweet
-const tweetForm = document.querySelector('#tweet-form');
+// DOM elements
+const tweetInput = document.querySelector('#tweet-input');
+const tweetButton = document.querySelector('#tweet-button');
+const topicList = document.querySelector('#topic-list');
+const tweetList = document.querySelector('#tweet-list');
 
-// Ajouter un gestionnaire d'événement pour le soumission du formulaire
-tweetForm.addEventListener('submit', event => {
-  // Empêcher la soumission du formulaire par défaut
-  event.preventDefault();
+// Topics
+const topics = {
+  food: ['I love pizza', 'Burgers are the best', 'Sushi is my favorite'],
+  sports: ['Basketball is my favorite sport', 'Football is the best', 'Baseball is boring'],
+  music: ['I love listening to jazz', 'Rock and roll is the best', 'Hip hop is my favorite']
+};
 
-  // Récupérer le texte du tweet entré par l'utilisateur
-  const tweetText = document.querySelector('#tweet-text').value;
-
-  // Envoyer une requête POST à l'API pour ajouter le nouveau tweet
-  fetch('/api/tweets', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ text: tweetText })
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Ajouter le nouveau tweet à la liste des tweets affichés sur la page
-    // ...
-  })
-  .catch(error => {
-    console.error('Erreur lors de l\'ajout du tweet :', error);
-  });
+// Event listeners
+tweetButton.addEventListener('click', () => {
+  const tweetText = tweetInput.value.trim();
+  if (tweetText !== '') {
+    addTweet(tweetText);
+    tweetInput.value = '';
+  }
 });
+
